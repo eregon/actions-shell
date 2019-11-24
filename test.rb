151bc6da23cc73b53cc244b8b91c609a5ed31418
Benoit Daloze <eregontp@gmail.com>
2019-11-24 13:52:46 +0100

    sassc

--- test.rb
+++ test.rb
@@ -1,1 +1,13 @@
 pp RbConfig::CONFIG.sort.to_h
+puts
+
+require 'sassc'
+
+spec = Gem.loaded_specs["sassc"]
+gem_root = spec.gem_dir
+libsass = Dir.glob("#{gem_root}/lib/sassc/libsass.*").first
+p libsass
+system('file', libsass)
+
+puts
+system 'file /usr/lib/libssl*'
