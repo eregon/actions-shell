c696d70e9fecab47e9e246b6d8ec812e703225e9
Benoit Daloze <eregontp@gmail.com>
2019-11-24 14:11:25 +0100

    mkconfig

--- test.rb
+++ test.rb
@@ -1,14 +1,6 @@
+puts RUBY_DESCRIPTION
+puts RUBY_PLATFORM
+puts
 pp RbConfig::CONFIG.sort.to_h
 puts
-
-require 'sassc'
-
-spec = Gem.loaded_specs["sassc"]
-gem_root = spec.gem_dir
-libsass = Dir.glob("#{gem_root}/lib/sassc/libsass.*").first
-p libsass
-system('file', libsass)
-system('otool', '-hv', libsass)
-
-puts
-system 'otool -hv /usr/lib/libssl.dylib'
+pp RbConfig::MAKEFILE_CONFIG.sort.to_h
