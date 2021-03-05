049e4b4e0710f33ce67e6e07854cb4e691e19978
Benoit Daloze <eregontp@gmail.com>
2021-03-05 11:53:11 +0100

    print RbConfig

--- test.rb
+++ test.rb
@@ -2,20 +2,9 @@ puts RUBY_DESCRIPTION
 puts RUBY_PLATFORM
 
 puts
-puts "ENV"
-pp ENV.sort.to_h
-
-puts
-puts "Signal.list"
-pp Signal.list
-
-puts
 puts "RbConfig::CONFIG"
 pp RbConfig::CONFIG.sort.to_h
 
 puts
 puts "RbConfig::MAKEFILE_CONFIG"
 pp RbConfig::MAKEFILE_CONFIG.sort.to_h
-
-p File.stat(__FILE__).uid
-p File.stat(__FILE__).gid
